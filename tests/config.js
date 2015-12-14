37a041f5bcbe6685ea4f9c118c31f62739a4908b
hosoi <hosoi@appland.co.jp>
2015-12-14 23:22:16 +0900

    init (fork)

---
+++ tests/config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var path = require('path'),
+    AWS = require('aws-sdk');
+
+// Require ENV vars, can also set ENV vars in your IDE
+require('dotenv').config({path: path.join(__dirname, '.env'), silent: true});
+
+process.env.JAWS_VERBOSE = true;
+
+var config = {
+  name: 'test-prj',
+  domain: 'test-prj.com',
+  notifyEmail: 'tester@jawsstack.com',
+  stage: 'unittest',
+  region: 'us-east-1',
+  stage2: 'unittest2',
+  region2: 'us-west-2',
+  iamRoleArnApiGateway: process.env.TEST_JAWS_APIGATEWAY_ROLE,
+  iamRoleArnLambda: process.env.TEST_JAWS_LAMBDA_ROLE,
+  profile: process.env.TEST_JAWS_PROFILE,
+  noExecuteCf: (process.env.TEST_JAWS_NO_EXE_CF != "false"),
+};
+
+AWS.config.credentials = new AWS.SharedIniFileCredentials({
+  profile: config.profile,
+});
+
+AWS.config.update({
+  region: config.region,
+});
+
+module.exports = config;
